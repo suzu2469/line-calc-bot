27855e089573f190cddd4258f6dea6fee14c66c0
Souma Suzuki <soumasuzuki@SoumanoMacBook-puro.local>
2018-08-23 16:14:24 +0900

    feat: first commit

---
+++ app.ts
@@ -0,0 +1,30 @@
+import * as line from '@line/bot-sdk'
+import * as express from 'express'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+const config: line.ClientConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
+}
+
+const app = express()
+app.post('/webhook', (req, res, next) => {
+  return Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+})
+
+const client = new line.Client(config)
+const handleEvent = (event: line.WebhookEvent) => {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text
+  })
+}
+
+app.listen(3000)
